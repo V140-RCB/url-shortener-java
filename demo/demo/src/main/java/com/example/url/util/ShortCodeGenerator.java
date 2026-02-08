bff3ce74c7a32cae5982da0c48743c4f43d8902e
V140-RCB <vrevankar140@gmail.com>
2026-02-08 10:13:14 +0000

    URL generating

--- demo/demo/src/main/java/com/example/url/util/ShortCodeGenerator.java
+++ demo/demo/src/main/java/com/example/url/util/ShortCodeGenerator.java
@@ -1,1 +1,8 @@
 package com.example.url.cloud.util;
+
+@Component
+public class ShortCodeGenerator {
+    public String generate(String url) {
+        return Integer.toHexString(url.hashCode()).substring(0, 6);
+    }
+}
